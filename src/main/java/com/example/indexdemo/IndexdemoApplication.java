e20aaa74a609deeb0fe97b2fb1a0f006db8c00e1
BlackCat <113409167+ChuHyeonJeong75@users.noreply.github.com>
2022-11-09 10:16:33 +0900

    Initial commit

---
+++ src/main/java/com/example/indexdemo/IndexdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.indexdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndexdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndexdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/indexdemo/IndexdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.indexdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndexdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
